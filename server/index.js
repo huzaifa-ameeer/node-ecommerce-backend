b3630a06033a20354fe79fea7c29e7a5fd37959e
Muhammad Huzaifa Ameer <huzaifahnbhai098@gmail.com>
2026-09-15 11:21:11 +0500

    added some middlerwares

--- server/index.js
+++ server/index.js
@@ -1,16 +1,21 @@
-import dotenv from "dotenv"
-import express from "express"
+import dotenv from "dotenv";
+import express from "express";
+import cors from "cors"
+import chalk from "chalk";
 
-dotenv.config()
+dotenv.config();
 
-const app = express()
+const app = express();
+app.use(express.json())
+app.use(cors())
 
-const port = process.env.PORT || 3000
 
-app.get("/test", (req, res)=>{
-    return res.send("Hello from server")
-})
+const port = process.env.PORT || 3000;
 
-app.listen(port, ()=>{
-    console.log(`Server running on port: ${port}`)
-})
+app.get("/test", (req, res) => {
+  return res.send("Hello from server");
+});
+
+app.listen(port, () => {
+  console.log(chalk.bgBlue.white(`Server running on port: ${port}`));
+});
