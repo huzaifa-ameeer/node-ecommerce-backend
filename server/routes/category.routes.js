f7b195de041aaedeb6c16d3b3339b0c2232a5f03
Muhammad Huzaifa Ameer <huzaifahnbhai098@gmail.com>
2026-09-23 23:18:00 +0500

    fixed: added admin only role for creating products, categories etc

--- server/routes/category.routes.js
+++ server/routes/category.routes.js
@@ -1,5 +1,5 @@
 import express from "express"
-import isAuth from "../middleware/auth.middleware.js"
+import isAuth, { isAdmin } from "../middleware/auth.middleware.js"
 import { createCategoryController, deleteCategoryController, getAllCategoriesController, updateCategoryController } from "../controllers/category.controller.js"
 
 const router = express.Router()
@@ -7,12 +7,12 @@ const router = express.Router()
 //routes
 
 //create category (POST)
-router.post("/create", isAuth, createCategoryController)
+router.post("/create", isAuth, isAdmin, createCategoryController)
 //get all categories (GET)
 router.get("/get-all", getAllCategoriesController)
 //delete category (DELETE)
-router.delete("/delete/:id", isAuth, deleteCategoryController)
+router.delete("/delete/:id", isAuth, isAdmin,deleteCategoryController)
 //update category (PUT)
-router.put("/update/:id", isAuth, updateCategoryController)
+router.put("/update/:id", isAuth, isAdmin,updateCategoryController)
 
 export default router
--- server/routes/product.routes.js
+++ server/routes/product.routes.js
@@ -8,7 +8,7 @@ import {
   updateProductController,
   updateProductImageController,
 } from "../controllers/product.controller.js";
-import isAuth from "../middleware/auth.middleware.js";
+import isAuth, { isAdmin } from "../middleware/auth.middleware.js";
 import singleUpload from "../middleware/multer.js";
 
 const router = express.Router();
@@ -20,17 +20,26 @@ router.get("/get-all", getAllProductsController);
 //get single product (GET)
 router.get("/:id", getSingleProductController);
 // //create product (POST)
-router.post("/create", isAuth, singleUpload, createProductController);
+router.post("/create", isAuth, isAdmin, singleUpload, createProductController);
 //update product (PUT)
-router.put("/update/:id", isAuth, updateProductController);
+router.put("/update/:id", isAuth, isAdmin, updateProductController);
 //update product image (PUT)
-router.put("/update-image/:id",
-  isAuth, singleUpload, updateProductImageController,);
+router.put(
+  "/update-image/:id",
+  isAuth,
+  isAdmin,
+  singleUpload,
+  updateProductImageController,
+);
 //delete product image(DEL)
-router.delete("/delete-product-image/:id/:public_id", isAuth, deleteProductImageController)  
+router.delete(
+  "/delete-product-image/:id/:public_id",
+  isAuth,
+  isAdmin,
+  deleteProductImageController,
+);
 //delete product (DEL)
-router.delete("/delete-product/:id", isAuth, deleteProductController)
-
+router.delete("/delete-product/:id", isAuth, isAdmin, deleteProductController);
 
 //exports
 export default router;
